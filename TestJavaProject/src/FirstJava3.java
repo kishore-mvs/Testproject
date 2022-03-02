d18fb618430c21a7013ee6691a1e3c515619beb4
Kishore <Kishorem_vs@yahoo.co.in>
2022-03-03 00:06:23 +0530

    Commit3

---
+++ TestJavaProject/src/FirstJava3.java
@@ -0,0 +1,15 @@
+
+public class FirstJava3 {
+	
+	public  void sample2()
+	{
+		System.out.println("Sample prog2");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+FirstJava3 FJ= new FirstJava3();
+FJ.sample2();
+	}
+
+}
