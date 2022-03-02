8a6c6c63da456b4a12745fb0cf043aed04b14b27
Kishore <Kishorem_vs@yahoo.co.in>
2022-03-02 22:41:35 +0530

    Firt commit

---
+++ TestJavaProject/src/FirstJava.java
@@ -0,0 +1,15 @@
+
+public class FirstJava {
+	
+	public  void sample()
+	{
+		System.out.println("Sample prog");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+FirstJava FJ= new FirstJava();
+FJ.sample();
+	}
+
+}
